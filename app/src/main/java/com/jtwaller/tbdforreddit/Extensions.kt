99376eb2e2ec2f4b0d379092f59a9a2079c9ce5a
Jack Waller <ucdwaller@gmail.com>
2019-03-18 09:01:33 -0700

    Use standard Java string library for string format calls

--- app/src/main/java/com/jtwaller/tbdforreddit/Extensions.kt
+++ app/src/main/java/com/jtwaller/tbdforreddit/Extensions.kt
@@ -7,11 +7,11 @@ fun Period.printLongestUnit(c: Context): String {
     val tmp = this.normalizedStandard()
     val res = c.resources
 
-    return if (tmp.years > 0) kotlin.String.format(res.getString(R.string.year_abbrev), tmp.years)
-    else if (tmp.months > 0) kotlin.String.format(res.getString(R.string.month_abbrev), tmp.months)
-    else if (tmp.weeks > 0) kotlin.String.format(res.getString(R.string.week_abbrev), tmp.weeks)
-    else if (tmp.days > 0) kotlin.String.format(res.getString(R.string.day_abbrev), tmp.days)
-    else if (tmp.hours > 0) kotlin.String.format(res.getString(R.string.hour_abbrev), tmp.hours)
-    else if (tmp.minutes > 0) kotlin.String.format(res.getString(R.string.minute_abbrev), tmp.minutes)
+    return if (tmp.years > 0) String.format(res.getString(R.string.year_abbrev), tmp.years)
+    else if (tmp.months > 0) String.format(res.getString(R.string.month_abbrev), tmp.months)
+    else if (tmp.weeks > 0) String.format(res.getString(R.string.week_abbrev), tmp.weeks)
+    else if (tmp.days > 0) String.format(res.getString(R.string.day_abbrev), tmp.days)
+    else if (tmp.hours > 0) String.format(res.getString(R.string.hour_abbrev), tmp.hours)
+    else if (tmp.minutes > 0) String.format(res.getString(R.string.minute_abbrev), tmp.minutes)
     else kotlin.String.format(res.getString(R.string.second_abbrev), tmp.seconds)
 }
