8b3dab534351acbc8b258b86e63bb85bd7c8556e
Jack Waller <ucdwaller@gmail.com>
2019-03-20 19:40:33 -0700

    Implement barebones viewpager for main activity
    Starting point for feeding the comment fragment in on link tap

--- app/src/main/java/com/jtwaller/tbdforreddit/MainActivity.kt
+++ app/src/main/java/com/jtwaller/tbdforreddit/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.jtwaller.tbdforreddit
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager.widget.ViewPager
+import com.jtwaller.tbdforreddit.ui.adapters.MainFragmentPagerAdapter
 import net.danlew.android.joda.JodaTimeAndroid
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
+
+    private lateinit var mViewPager: ViewPager
+    private lateinit var mPagerAdapter: MainFragmentPagerAdapter
 
     companion object {
         const val TAG = "MainActivity"
@@ -15,6 +21,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         JodaTimeAndroid.init(this)
+
+        mPagerAdapter = MainFragmentPagerAdapter(supportFragmentManager)
+        mViewPager = findViewById(R.id.pager)
+        mViewPager.adapter = mPagerAdapter
+
+        mViewPager.addOnPageChangeListener(
+                object : ViewPager.SimpleOnPageChangeListener() {
+                    override fun onPageSelected(position: Int) {
+                        Log.d(TAG, ": onPageSelected $position")
+                        mViewPager.currentItem = position
+                    }
+                }
+        )
     }
 
 }
