fafad9e74e81a562abd52dd67d7baa87117955ae
Jack Waller <ucdwaller@gmail.com>
2019-04-30 15:58:08 -0700

    Basic navigation drawer implementation

--- app/src/main/java/com/jtwaller/tbdforreddit/MainActivity.kt
+++ app/src/main/java/com/jtwaller/tbdforreddit/MainActivity.kt
@@ -2,17 +2,26 @@ package com.jtwaller.tbdforreddit
 
 import android.content.*
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.android.material.navigation.NavigationView
 import com.jtwaller.tbdforreddit.debug.DebugUtils
 import com.jtwaller.tbdforreddit.models.RedditObjectData
 import com.jtwaller.tbdforreddit.services.MainBroadcastReceiver
 import com.jtwaller.tbdforreddit.ui.fragments.DetailFragment
 import net.danlew.android.joda.JodaTimeAndroid
+import java.lang.RuntimeException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBroadcastReceiver: MainBroadcastReceiver
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var navigationView: NavigationView
 
     companion object {
         const val TAG = "MainActivity"
@@ -30,7 +39,31 @@ class MainActivity : AppCompatActivity() {
                 .getInstance(this)
                 .registerReceiver(mBroadcastReceiver, IntentFilter(BUILD_FRAGMENT_ACTION))
 
-        DebugUtils.broadcastLinkData(this, "r/Frugal/comments/b8wwi4/frugal_tips_why_my_coworkers_make_fun_of_me/")
+        drawerLayout = findViewById(R.id.main_layout)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+
+        toggle.syncState()
+
+        // TODO - why does this setup the home button to pull our the drawer???
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navigationView = findViewById(R.id.main_navigation_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.sign_in -> Toast.makeText(this, "Sign in tapped", Toast.LENGTH_SHORT).show()
+                else -> throw RuntimeException("Invalid navigation menu item id")
+            }
+            true
+        }
+
+//        DebugUtils.broadcastLinkData(this, "r/Frugal/comments/b8wwi4/frugal_tips_why_my_coworkers_make_fun_of_me/")
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (toggle.onOptionsItemSelected(item)) return true
+        return super.onOptionsItemSelected(item)
     }
 
     fun createDetailFragment(parentObjectData: RedditObjectData) {
