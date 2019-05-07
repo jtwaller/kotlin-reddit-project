a0174904eeed45ec83f8e73dd80b215eeb6e4d29
Jack Waller <ucdwaller@gmail.com>
2019-05-07 13:57:41 -0700

    Move identity information to appropriate classes
    Move identity information and logic from MainActivity to RedditUser and
    OAuthToken classes

--- app/src/main/java/com/jtwaller/tbdforreddit/MainActivity.kt
+++ app/src/main/java/com/jtwaller/tbdforreddit/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.jtwaller.tbdforreddit
 
 import android.content.*
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
@@ -14,6 +11,7 @@ import com.google.android.material.navigation.NavigationView
 import com.jtwaller.tbdforreddit.debug.DebugUtils
 import com.jtwaller.tbdforreddit.models.OAuthToken
 import com.jtwaller.tbdforreddit.models.RedditObjectData
+import com.jtwaller.tbdforreddit.models.RedditUser
 import com.jtwaller.tbdforreddit.services.MainBroadcastReceiver
 import com.jtwaller.tbdforreddit.ui.fragments.DetailFragment
 import net.danlew.android.joda.JodaTimeAndroid
@@ -26,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var navigationView: NavigationView
 
-    var oAuthToken: OAuthToken? = null
+    val redditUser = RedditUser.instance
 
     companion object {
         const val TAG = "MainActivity"
@@ -57,39 +55,16 @@ class MainActivity : AppCompatActivity() {
         navigationView = findViewById(R.id.main_navigation_view)
         navigationView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.sign_in -> authorize()
+                R.id.sign_in -> redditUser.login(this)
                 else -> throw RuntimeException("Invalid navigation menu item id")
             }
             true
         }
 
         intent?.data?.let { uri ->
-            val authFragment = uri.fragment ?: throw RuntimeException("No fragment in Auth response")
-
-            val parameters = authFragment.split('&')
-
-            // Couldn't find an easy way to parse uri fragment parameters?
-            // E.g., uri.getQueryParameter(someKey) vs uri.getFragmentParameter(someKey)
-            val accessToken = parameters[0].split('=')[1]
-            val tokenType = parameters[1].split('=')[1]
-            val state = parameters[2].split('=')[1]
-            val expiresIn = parameters[3].split('=')[1]
-            val scope = parameters[4].split('=')[1]
-
-            if (tokenType != "bearer") {
-                Log.d(TAG, ": Invalid token type - ${tokenType}")
-                return
-            }
-
-            oAuthToken = OAuthToken(
-                    accessToken,
-                    tokenType,
-                    expiresIn.toInt(),
-                    state,
-                    scope
-            )
-
-            Log.d(TAG, ": We're logged in!  Current token -> $accessToken")
+            // TODO - graceful error handling
+            val authFragment = uri.fragment ?: throw RuntimeException("No fragment data in Auth API response")
+            redditUser.setOAuthToken(OAuthToken.fromApiFragment(authFragment))
         }
 //        DebugUtils.broadcastLinkData(this, "r/Frugal/comments/b8wwi4/frugal_tips_why_my_coworkers_make_fun_of_me/")
     }
@@ -112,39 +87,6 @@ class MainActivity : AppCompatActivity() {
                             R.anim.exit_to_right)
                     commit()
                 }
-    }
-
-    fun authorize() {
-        if (oAuthToken != null) {
-            Toast.makeText(this, "Valid auth token exists", Toast.LENGTH_SHORT).show()
-
-            // TODO - show username in drawer
-            return
-        }
-
-        // If not building, please create res/values/secrets.xml with your own client id!
-        val clientId = getString(R.string.client_id)
-        val responseType = "token"
-        val state = "UNIQUE_STATE"
-        val redirectUri = "app://open.tbdforreddit"
-        val scope = "submit,identity"
-        val duration = "temporary"
-
-        val authUrl = "https://m.reddit.com/api/v1/authorize.compact?" +
-                "client_id=$clientId" +
-                "&response_type=$responseType" +
-                "&state=$state" +
-                "&duration=$duration" +
-                "&redirect_uri=$redirectUri" +
-                "&scope=$scope"
-
-        Log.d(TAG, ": $authUrl")
-
-        val intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse(authUrl)
-
-        startActivity(intent)
-
     }
 
 }
--- app/src/main/java/com/jtwaller/tbdforreddit/models/OAuthToken.kt
+++ app/src/main/java/com/jtwaller/tbdforreddit/models/OAuthToken.kt
@@ -1,9 +1,60 @@
 package com.jtwaller.tbdforreddit.models
 
+import java.lang.Exception
+import java.lang.RuntimeException
+import java.util.concurrent.TimeUnit
+
 class OAuthToken(
         val access_token: String,
         val token_type: String,
-        val expires_in: Int,
+        val expiresInSeconds: Long,
         val scope: String,
         val refresh_token: String
-)
+) {
+
+    private val expireTimeUtc: Long
+
+    init {
+        expireTimeUtc = System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(expiresInSeconds)
+    }
+
+    companion object {
+        fun fromApiFragment(authFragment: String): OAuthToken {
+            try {
+                val parameters = authFragment.split('&')
+
+                // Couldn't find an easy way to parse uri fragment parameters?
+                // E.g., uri.getQueryParameter(someKey) vs uri.getFragmentParameter(someKey)
+                val accessToken = parameters[0].split('=')[1]
+                val tokenType = parameters[1].split('=')[1]
+                val state = parameters[2].split('=')[1]
+                val expiresInSeconds = parameters[3].split('=')[1]
+                val scope = parameters[4].split('=')[1]
+
+                if (tokenType != "bearer") {
+                    throw RuntimeException("Invalid token type - ${tokenType}")
+                }
+
+                return OAuthToken(
+                        accessToken,
+                        tokenType,
+                        expiresInSeconds.toLong(),
+                        state,
+                        scope)
+            } catch (e: Exception) {
+                // TODO - validate auth fragment and handle errors
+
+                e.printStackTrace()
+                throw RuntimeException("Invalid auth fragment - $authFragment")
+            }
+        }
+    }
+
+    fun getAccessToken(): String {
+        return access_token
+    }
+
+    fun isExpired(): Boolean {
+        return (expireTimeUtc > System.currentTimeMillis())
+    }
+}
